b9516288e3fce96944f53ed35f2805cc439fa04d
Srikar Nivas Reddy Yerra <147457199+Srikarnivas@users.noreply.github.com>
2025-05-30 17:17:30 +0530

    feat: implement create_application method to store asset ID on creation

--- projects/medicalnft-contracts/smart_contracts/medi_nft/contract.py
+++ projects/medicalnft-contracts/smart_contracts/medi_nft/contract.py
@@ -1,31 +1,14 @@
 from algopy import *
+
 from algopy.arc4 import abimethod
 
 
-class NftTransfer(ARC4Contract):
+class NFTContract(ARC4Contract):
     assetid: UInt64
+    access_holder: Account
+    access_expires_at: UInt64
+    access_active: UInt64
 
     @abimethod(allow_actions=["NoOp"], create="require")
-    def create_application(self, asset_id: Asset ) -> None:
+    def create_application(self, asset_id: Asset) -> None:
         self.assetid = asset_id.id
-    
-    @abimethod
-    def asset_opt_in(self, asset: Asset) -> None:
-        assert asset.id == self.assetid
-
-        itxn.AssetTransfer(
-            asset_receiver=Global.current_application_address,
-            xfer_asset=asset,
-            asset_amount=0,
-            fee=0,
-        ).submit()
-
-    @abimethod
-    def asset_transfer(self, asset: Asset, receiver: Account, amount: UInt64) -> None:
-        assert asset.id == self.assetid
-
-        itxn.AssetTransfer(
-            asset_receiver=receiver,
-            xfer_asset=asset,
-            asset_amount=amount
-            ).submit()
