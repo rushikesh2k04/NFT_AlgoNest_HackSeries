94c3c99524cf9d3ac0e60d91c518a2168c8c94b3
Srikar Nivas Reddy Yerra <147457199+Srikarnivas@users.noreply.github.com>
2025-05-30 19:50:15 +0530

    feat: change NFT asset clawback address to revoke smart contract.

--- projects/medicalnft-frontend/src/Home.tsx
+++ projects/medicalnft-frontend/src/Home.tsx
@@ -150,7 +150,81 @@ const Home: React.FC = () => {
           )
         }
 
-         <div className="flex flex-col gap-2">
+        {/* <MethodCall
+  methodFunction={async () => {
+    if (assetId === 0n) {
+      alert("Create the Asset first!");
+      return;
+    }
+    setLoading(true);
+    try {
+      const transfer = methods.transferAsset(
+        algorand,
+        nftClient,
+        TransactionSigner,
+        activeAddress!,
+        assetId
+      );
+      await transfer();
+      alert("Transfer successful!");
+    } catch (error) {
+      alert("Transfer failed: ");
+    } finally {
+      setLoading(false);
+    }
+  }}
+  text={loading ? "Processing..." : "Get the asset"}
+/> */}
+          {/* <MethodCall
+          methodFunction={ async () => {
+            if (assetId === 0n) {
+            alert("Create the Asset first!");
+            return;
+            }
+          setLoading(true);
+          try {
+              const optin = methods.optIn(
+              algorand,
+              TransactionSigner,
+              activeAddress!,
+              assetId
+            );
+          await optin();
+          alert("Opt in successful!");
+          } catch (error) {
+          alert("Opt in failed: ");
+          } finally {
+          setLoading(false);
+          }
+            }}
+            text = "Opt in"
+            /> */}
+
+          {/* <MethodCall
+          methodFunction={ async () =>{
+            setLoading(true);
+            try{
+              const transfering = methods.assetTransferfromapp(
+                algorand,
+                nftClient,
+                TransactionSigner,
+                String(algosdk.getApplicationAddress(appId)),
+                assetId,
+                activeAddress!,
+              );
+              await transfering();
+              alert("Asset"+assetId+"transfered"+activeAddress)
+            }
+            catch(error) {
+              console.log("Asste transfer is failed");
+            } finally {
+              setLoading(false);
+            }
+          }}
+          text = "Transfer"
+          /> */}
+
+          <div className="flex flex-col gap-2">
             <label>
              App ID:
               <input
@@ -208,6 +282,7 @@ const Home: React.FC = () => {
   text={loading ? "Transferring..." : "Transfer from App"}
 />
 
+
 <MethodCall
   methodFunction={async () => {
     setLoading(true);
@@ -246,3 +321,35 @@ const Home: React.FC = () => {
             </div>
           )
         }
+      <MethodCall
+      methodFunction={ async () => {
+        try{
+          const parsedAssetId = BigInt(inputAssetId);
+          const parsedAppId = BigInt(revokeAppId);
+          const clawbackAddress = await revokeMethods.changeClawBack(
+            algorand,
+            TransactionSigner,
+            parsedAppId,
+            activeAddress!,
+            parsedAssetId
+          )
+
+          const result = await clawbackAddress();
+          console.log(result," Successfull")
+        } catch (err){
+          console.error(err);
+          alert(" Failed to Change the cLawback address:");
+        } finally {
+
+        }
+      }}
+      text = "change claw back"/>
+
+
+        <ConnectWallet openModal={openWalletModal} closeModal={toggleWalletModal} />
+      </div>
+    </div>
+  )
+}
+
+export default Home
