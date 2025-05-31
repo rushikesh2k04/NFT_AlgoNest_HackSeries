048d729555273fbb035a239441f3575d899a08ea
Rushikesh <151829344+rushikesh2k04@users.noreply.github.com>
2025-05-31 11:19:22 +0530

    feat: add opt_in_to_asset method for smart contract asset opt-in

--- projects/medicalnft-contracts/smart_contracts/medi_nft/contract.py
+++ projects/medicalnft-contracts/smart_contracts/medi_nft/contract.py
@@ -12,3 +12,19 @@ class NFTContract(ARC4Contract):
     @abimethod(allow_actions=["NoOp"], create="require")
     def create_application(self, asset_id: Asset) -> None:
         self.assetid = asset_id.id
+ # opt in to the asset into the smart contract
+    @abimethod()
+    def opt_in_to_asset(self, mbrpay: gtxn.PaymentTransaction) -> None:
+        assert Txn.sender == Global.creator_address
+        assert not Global.current_application_address.is_opted_in(Asset(self.assetid))
+
+        assert mbrpay.receiver == Global.current_application_address
+
+        assert mbrpay.amount == Global.min_balance + Global.asset_opt_in_min_balance
+
+        itxn.AssetTransfer(
+            xfer_asset= self.assetid,
+            asset_receiver= Global.current_application_address,
+            asset_amount= 0,
+            fee=0,
+        ).submit()
